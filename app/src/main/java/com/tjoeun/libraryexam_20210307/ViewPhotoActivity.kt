2b7f82ee01231bc0afe3e6515fe07f9c81df9294
Eun-Hee <hd96010@daum.net>
2021-03-07 15:25:29 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/libraryexam_20210307/ViewPhotoActivity.kt
@@ -0,0 +1,22 @@
+package com.tjoeun.libraryexam_20210307
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : BaseActivity() {
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+        setupEvents()
+        setValues()
+
+    }
+}
